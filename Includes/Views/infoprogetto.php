47ede203fd9eeec2f795dfac4b476f5066d727a2
mattiabruno <mattiz94nu@gmail.com>
2016-02-19 20:41:12 +0100

    Update infoprogetto.php

--- Includes/Views/infoprogetto.php
+++ Includes/Views/infoprogetto.php
@@ -1,32 +1,32 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
         <title></title>
     </head>
     <body>
 		
         <h2> Descrizione dell'applicazione </h2>
         <p>
-            L’applicazione supporta l'acquisto di film sul web. 
-			Le funzionalità dell'admin comprendono la creazione, la modifica e l'eliminazione di film dal database.
-            Le funzionalità dell'utente comprendono l'aggiunta dei film sul carrello, e l'acquisto di quest'utlimi.
-			I dati presenti all'interno del database sono i seguenti:
+        Il sito che ho realizzato gestisce un negozio di film.
+	Le funzionalità dell'admin comprendono la creazione, la modifica e l'eliminazione di film dal database.
+        Le funzionalità dell'utente comprendono l'aggiunta dei film sul carrello, e l'acquisto di quest'utlimi.
+        I dati presenti all'interno del database sono i seguenti:
+	    
         </p>
         <ul>
             <li>Id_utente, nome, password della tabella utente</li>
             <li>Id_film, titolo, prezzo, durata film, descrizione e magazzino della tabella film </li>
-			<li>Id_carrello, film_id e utente_id per la tabella carrello</li>
+	    <li>Id_carrello, film_id e utente_id per la tabella carrello</li>
         </ul>
-
         <p>Inoltre, l'utente &egrave; in grado di visualizzare 
-            i film presenti nel database direttamente dalla home, dove vengono
-			visualizzati i seguenti dati:
+        i film presenti nel database direttamente dalla home, dove vengono
+	visualizzati i seguenti dati:
         </p>
         <ul>
-			<li>L'immagine relativa al film caricato nel database tramite una url.</li>
+            <li>L'immagine relativa al film caricato nel database tramite una url.</li>
             <li>Un titolo</li>
-			<li>La durata del film</li>
+	    <li>La durata del film</li>
             <li>Il prezzo</li>
             <li>La quantità disponibile in magazzino</li>
             <li>Una descrizione</li>			
@@ -37,10 +37,13 @@
             <li>Utilizzo di HTML e CSS</li>
             <li>Utilizzo di PHP e MySQL</li>
             <li>Utilizzo del pattern MVC </li>
+            <li>Utilizzo di jQuery per la realizzazione dello slideshow, nel quale vengono visualizzate varie immagini caricate nella cartella
+            immagini.</li>
             <li>Due ruoli (utente e admin)</li>
-            <li>Transazione per la compravendità dei film</li>
-            <li>Caricamento ajax dei dati relativi al film da eliminare(ruolo admin)</li>
-
+            <li>Transazioni:</li>
+            <li>Lato User: Transazione per la compravendità dei film, funzione per l'aggiunta e l'eliminazione di un film dal carrello.  </li>
+            <li>Lato Admin: Funzione per l'aggiunta, l'eliminazione e la modifica di un film.</li>
+            <li>Caricamento ajax dei dati relativi al film da eliminare, i dati vengono estrappolati dal database.(ruolo admin)</li>
         </ul>
     </ul>
 

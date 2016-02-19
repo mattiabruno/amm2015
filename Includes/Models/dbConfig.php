80e33ac17467935ce05341cc75f376ef6f4c7a13
mattiabruno <mattiz94nu@gmail.com>
2016-02-19 19:47:43 +0100

    Update dbConfig.php

--- Includes/Models/dbConfig.php
+++ Includes/Models/dbConfig.php
@@ -2,9 +2,9 @@
 
 define ("DB_HOST", "localhost"); // set database host
 
-define ("DB_USER", ""); // set database user
+define ("DB_USER", "brunoMattia"); // set database user
 
-define ("DB_PASS", ""); // set database password
+define ("DB_PASS", "gnu4867"); // set database password
 
 define ("DB_NAME", "amm15_brunoMattia"); // set database name
 

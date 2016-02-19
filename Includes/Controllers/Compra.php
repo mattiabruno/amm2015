11fb73a98c3e6174f197acc20417bdb8f52fe928
mattiabruno <mattiz94nu@gmail.com>
2016-02-19 19:19:19 +0100

    amm2015

--- Includes/Controllers/Compra.php
+++
@@ -1,7 +0,0 @@
-<?php
-include '../Models/Database.php';
-$auth = new Database();
-$controllo = $auth->Compra();
-header("Location:../Views/spedizione.php");
-?>
-
--- Includes/Models/dbConfig.php
+++
@@ -1,1 +0,0 @@
-<?phpdefine ("DB_HOST", "localhost"); // set database hostdefine ("DB_USER", "root"); // set database userdefine ("DB_PASS", "bruno"); // set database passworddefine ("DB_NAME", "dbfilm"); // set database name?>
